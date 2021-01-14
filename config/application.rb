9b92961c1acaa579c8019f3f579498e60ca8d4c1
jtduchesne <jtduchesne@gmail.com>
2021-01-14 01:11:19 -0500

    Français comme langue par défaut

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,10 @@ module Jtduchesne
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # Internationalization (I18n)
+    I18n.available_locales = [:en, :fr]
+    I18n.default_locale = :fr
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
