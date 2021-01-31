e4e9d6e06c2f608a1d7fa7dfc8830523858cc0fa
jtduchesne <jtduchesne@gmail.com>
2021-01-31 01:04:34 -0500

    Create #relative_date helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,14 @@ module ApplicationHelper
   def active_if(url)
     "active" if current_page?(url)
   end
+  
+  def relative_date(time)
+    date = time.to_date
+    if date >= Date.yesterday
+      time_ago = l(time, format: :short)
+    else
+      time_ago = time_ago_in_words(time)
+    end
+    t('relative_date', scope: 'datetime.distance_in_words', count: (Date.today - date), time_ago: time_ago)
+  end
 end
